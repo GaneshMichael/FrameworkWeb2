7c97783ffaf7e6693becdbdc4c629b01e5fa607b
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2024-02-26 15:41:52 +0100

    registerModel en Model
    rules toegevoegd

--- Views/register.php
+++ Views/register.php
@@ -3,28 +3,24 @@
 <form action="" method="post">
     <div class="form-group">
         <label for="name" class="form-label">Name</label>
-        <input type="name" class="form-control" id="name">
+        <input type="text" class="form-control" name="firstname">
     </div>
     <div class="form-group">
-        <label for="lastname" class="form-label">Lastname</label>
-        <input type="lastname" class="form-control" id="lastname">
+        <label class="form-label">Lastname</label>
+        <input type="text" class="form-control" name="lastname">
     </div>
     <div class="form-group">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label>Email address</label>
+        <input type="text" class="form-control" name="email" " aria-describedby="emailHelp">
     </div>
     <div class="form-group">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label >Password</label>
+        <input type="text" class="form-control" name="password">
     </div>
     <div class="form-group">
-        <label for="repeatPassword" class="form-label">Repeat Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label>Confirm Password</label>
+        <input type="text" class="form-control" name="confirmPassword">
     </div>
-    <div class="form-group">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Verify register</label>
-    </div>
+
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
