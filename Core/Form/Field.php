dec0b628cdb45da283272a3c020c1bcf0c2c36c1
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2024-02-27 11:30:55 +0100

    form handling

--- Core/Form/Field.php
+++ Core/Form/Field.php
@@ -6,11 +6,16 @@ use TCG\Core\Model;
 
 class Field
 {
+    public const TYPE_TEXT = 'text';
+    public const TYPE_PASSWORD = 'text';
+    public const TYPE_NUMBER = 'number';
+    public string $type;
     public Model $model;
     public string $attribute;
 
     public function __construct(Model $model, string $attribute)
     {
+        $this->type = self::TYPE_TEXT;
         $this->model = $model;
         $this->attribute = $attribute;
     }
@@ -20,15 +25,26 @@ class Field
         return sprintf('
             <div class="form-group">
                 <label>%s</label>
-                <input type="text" name="%s" value="%s" class="form-control%s">
+                <input type="%s" name="%s" value="%s" class="form-control%s">
+                <div class="invalid-feedback">
+                    %s
+                </div>            
             </div> 
+            
         ',
             $this->attribute,
+            $this->type,
             $this->attribute,
             $this->model->{$this->attribute},
             $this->model->hasError($this->attribute) ? ' is-invalid' : '',
             $this->model->getFirstError($this->attribute)
         );
+    }
+
+    public function passwordField()
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
     }
 
 }
--- Views/register.php
+++ Views/register.php
@@ -2,11 +2,17 @@
 
 <?php $form = TCG\Core\form\Form::begin('', 'post')  ?>
 
-<?php echo $form->field($model, 'firstname') ?>
-<?php echo $form->field($model, 'lastname') ?>
+<div class="row">
+    <div class="col">
+        <?php echo $form->field($model, 'firstname') ?>
+    </div>
+    <div class="col">
+        <?php echo $form->field($model, 'lastname') ?>
+    </div>
+</div>
 <?php echo $form->field($model, 'email') ?>
-<?php echo $form->field($model, 'password') ?>
-<?php echo $form->field($model, 'confirmPassword') ?>
+<?php echo $form->field($model, 'password')->passwordField() ?>
+<?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
 
 <button type="submit" class="btn btn-primary">Submit</button>
 
