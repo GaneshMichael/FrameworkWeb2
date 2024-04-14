5c8f16a97dc5c05dcd47c4896da0e47e9c3791b9
Michael Ganesh <m.a.s.ganesh@st.hanze.nl>
2024-04-14 20:22:05 +0200

    Update decks.php

--- Views/decks.php
+++ Views/decks.php
@@ -11,17 +11,17 @@ $currentUrl = $_SERVER['REQUEST_URI'];
 ?>
 
 <?php if (Auth::isPremium() || Auth::isAdmin()): ?>
-    <h2>Stel je deck samen</h2>
+    <h2 class="text-center">Stel je deck samen</h2>
     <a class="btn btn-primary" href="/decks/newDeck" role="button">Nieuw Deck Aanmaken</a>
 <?php elseif(Auth::isFree()): ?>
-    <h2>Het samenstellen van decks is een premium feature</h2>
-    <h2>Upgrade naar premium om decks te kunnen samenstellen</h2>
+    <h2 class="text-center">Het samenstellen van decks is een premium feature</h2>
+    <h2 class="text-center">Upgrade naar premium om decks te kunnen samenstellen</h2>
     <a class="btn btn-primary" href="/premium" role="button">Upgrade</a>
 <?php else: ?>
 
 <?php endif; ?>
 
-<h2>Alle decks</h2>
+<h2 class="text-center">Alle decks</h2>
 
 <table class="table">
     <thead>
